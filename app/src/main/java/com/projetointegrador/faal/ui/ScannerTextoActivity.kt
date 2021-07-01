784344e4f890122fbb7fb61a7f30d66305bae003
Thiago P. Martinez <thiago.pereira.ti@gmail.com>
2021-07-01 18:48:38 -0400

    chore: Enviar imagem para o Cloud Storage

--- app/src/main/java/com/projetointegrador/faal/ui/ScannerTextoActivity.kt
+++ app/src/main/java/com/projetointegrador/faal/ui/ScannerTextoActivity.kt
@@ -1,25 +1,36 @@
 package com.projetointegrador.faal.ui
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.ImageFormat
 import android.os.Bundle
-import androidx.camera.core.Camera
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.Preview
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleOwner
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.projetointegrador.faal.R
 import com.projetointegrador.faal.databinding.ActivityScannerTextoBinding
+import com.projetointegrador.faal.utils.YuvToRgbConverter
+import java.io.ByteArrayOutputStream
+import java.util.*
 
 class ScannerTextoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityScannerTextoBinding
     private lateinit var cameraProvider: ProcessCameraProvider
+    private lateinit var imageCapture: ImageCapture
     private lateinit var camera: Camera
+
+    private val storage = Firebase.storage
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +53,7 @@ class ScannerTextoActivity : AppCompatActivity() {
         ActivityCompat.requestPermissions(this, permissions, 200)
     }
 
+    @SuppressLint("RestrictedApi", "UnsafeOptInUsageError")
     private fun startCameraPreview() {
         cameraProvider = ProcessCameraProvider.getInstance(this).get()
 
@@ -50,8 +62,42 @@ class ScannerTextoActivity : AppCompatActivity() {
             .requireLensFacing(CameraSelector.LENS_FACING_BACK)
             .build()
 
+        imageCapture = ImageCapture.Builder()
+            .setBufferFormat(ImageFormat.YUV_420_888)
+            .build()
+
         preview.setSurfaceProvider(binding.cameraPreview.surfaceProvider)
-        camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+        camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview, imageCapture)
+
+        binding.btnTirarFoto.setOnClickListener {
+            binding.progress.isVisible = true
+
+            imageCapture.takePicture(ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageCapturedCallback() {
+
+                override fun onCaptureSuccess(image: ImageProxy) {
+                    image.use {
+                        uploadImage(it)
+                    }
+                }
+
+            })
+        }
+    }
+
+    @SuppressLint("UnsafeOptInUsageError")
+    private fun uploadImage(imageProxy: ImageProxy) {
+        val storageRef = storage.reference
+        val imageRef = storageRef.child("users/${FirebaseAuth.getInstance().currentUser!!.uid}/images/${Calendar.getInstance().timeInMillis}.jpg")
+
+        val bitmap = Bitmap.createBitmap(imageProxy.width, imageProxy.height, Bitmap.Config.ARGB_8888)
+        YuvToRgbConverter(this).yuvToRgb(imageProxy.image!!, bitmap)
+
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+
+        imageRef.putBytes(baos.toByteArray()).addOnSuccessListener {
+            binding.progress.isVisible = false
+        }
     }
 
     override fun onRequestPermissionsResult(
